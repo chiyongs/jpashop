431c7ceb07100bb7dd6d45b982d046fc7b0d2981
chiyongs <tlscldyd@gmail.com>
2021-02-25 22:34:52 +0900

    도메인 모델과 테이블 설계

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,12 +11,11 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
-  @Autowired MemberRepository memberRepository;
+  @Autowired
+  MemberRepository memberRepository;
 
   @Test
   @Transactional
